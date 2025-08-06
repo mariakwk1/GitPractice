5eef98b86ef451aaf3e93a70dd990689761b0fc5
maria <mariae8819550@gmail.com>
2025-08-06 11:28:15 -0400

    I added text

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text ("change 1!")
+            Text ("change 2!")
+            Image("cat")
+            
         }
         .padding()
     }
